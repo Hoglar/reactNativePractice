f51e704c973a846d4725ecbd16688a38e43c2ff3
Hoglar <martinhl__@hotmail.com>
2019-08-17 18:24:59 +0200

    Ok, lager en Pomodoro timer her!

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-let id = 0;
-
-const Todo = function(props) {
-    return (
-        <Text>{props.todo.text} {props.todo.id}</Text>
-    )
-}
 
 export default class App extends React.Component {
     constructor() {
         super()
-        this.state = {
-            todos: [],
-        }
-    }
 
-    addTodo() {
-        alert("time to", "Shine")
-        const text = "New Todo";
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {id: id++, text: text}
-            ]
-        })
     }
 
 
     render() {
         return (
-          <View style={styles.container}>
-            {this.state.todos.map(todo => <Todo key={todo.id} todo={todo}/>)}
-            <Button
-                title="Click Me"
-                onPress={this.addTodo.bind(this)}
-            />
-          </View>
+            <View>
+
+            </View>
+
         );
     }
 }
